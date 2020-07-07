2be796adee23d4fce6783c6cab853c0c5269ff76
Tim <tpadjen@gmail.com>
2020-07-06 18:51:10 -0600

    Fix express

--- src/current-song.ts
+++ src/current-song.ts
@@ -1,16 +1,12 @@
 require('dotenv').config()
 const SpotifyWebApi = require('spotify-web-api-node');
 
-const vars = {
+var spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   redirectUri: process.env.REDIRECT_URI,
   refreshToken: process.env.REFRESH_TOKEN
-};
-
-console.log(vars);
-
-var spotifyApi = new SpotifyWebApi(vars);
+});
 
 export interface Song {
   artist: string,
